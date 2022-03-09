eb0dd98d693f41e0dc3b5f5a90fd4519694bf260
Norfolk247 <36949024+Norfolk247@users.noreply.github.com>
2022-03-09 19:04:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const taskContent = document.getElementById('taskContent');
+const addButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList')
+
+class Task {
+    constructor(content) {
+        this.content = content
+        this.fav = false
+        this.link = ''
+    }
+
+    edit(){
+
+    }
+
+    favourite(){
+
+    }
+    delete(){
+
+    }
+    open(){
+
+    }
+    share(){
+
+    }
+}
