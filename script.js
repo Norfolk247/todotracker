b27896f5de52b1beb4391e9f8902d9e0cc200ad1
Norfolk247 <36949024+Norfolk247@users.noreply.github.com>
2022-03-11 02:59:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ class task {
         this.content = content
         this.completed = false
         this.fav = false
-        this.link = ''
     }
-
     edit(){
 
     }
@@ -22,21 +20,26 @@ class task {
     delete(){
 
     }
-    open(){
-
-    }
-    share(){
-
-    }
 }
 class tasklisthtml {
     purge(){
         taskList.innerHTML = ''
     }
+    createtask(object){
+        return `
+        <div class="listItem">
+            <div class="itemContent">${object.content}</div>
+            <div class="buttons">
+                <input type="checkbox">
+                <input type="image" class="delete">
+                <input type="image" class="favourite" ${object.fav ? 'true' : ''}>
+            </div>
+        </div>`
+    }
     fill(){
         if (tasks.length > 0) {
-            for (x of tasks) {
-                taskList.innerHTML += 
+            for (object of tasks) {
+                taskList.innerHTML += tasklisthtml.createtask(object)
             }
         }
     }
